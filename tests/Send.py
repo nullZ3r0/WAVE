d996b2a310916a5549760d3fecd223b6887996e8
nullZer0 <120856302+nullZ3r0@users.noreply.github.com>
2023-02-06 17:38:08 +0000

    Better communication
    Improved the UDP communication between JAVA and Python

--- tests/Send.py
+++ tests/Send.py
@@ -6,9 +6,21 @@
 import socket
 import time
 
+MESSAGE = b""
+
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-MESSAGE = input("Message? ").encode()
+MESSAGE += b"Gesture: "
+MESSAGE += input("Message? ").encode()
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+MESSAGE = b""
+
+MESSAGE += b"Voice: "
+MESSAGE += input("Message? ").encode()
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
